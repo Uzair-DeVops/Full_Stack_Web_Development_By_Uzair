9bbaaeaf45509cf01713471a3de7d33219517b13
Muhammad Uzair <uzairyasin83@gmail.com>
2025-04-13 00:43:08 +0500

    added dom 2 parts

---
+++ Frontend/JavaScript/DOM Basic/Targeting Methods/app.js
@@ -0,0 +1,10 @@
+var nameElement = document.getElementById("name");
+var firstname = nameElement.innerHTML;
+console.log(firstname);
+var age = document.getElementById("age").innerText;
+console.log(age);
+var docs = document.title;
+console.log(docs);
+// getelemetbyid
+// byclass
+// byname
---
+++ Frontend/JavaScript/DOM Basic/Targeting Methods/app.ts
@@ -0,0 +1,13 @@
+let nameElement = document.getElementById("name") as HTMLElement;
+let firstname :string = nameElement.innerHTML;
+console.log(firstname)
+
+let age : string = document.getElementById("age")!.innerText
+console.log(age)
+
+let docs  = document.title
+console.log(docs) 
+
+// getelemetbyid
+// byclass
+// byname
