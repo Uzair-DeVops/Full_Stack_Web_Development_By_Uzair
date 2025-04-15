95769e8c92a041d8ed519969435090f9e53a2ef7
Uzair-DeVops <uzairyasin83@gmail.com>
2025-04-15 08:57:50 -0700

    added some parts

---
+++ Frontend/JavaScript/Intermediate/01 Distructing Array/app.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("uzair yasin");
+let resturant = [
+    "uzair",
+    20,
+    true,
+    "yasin",
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+];
---
+++ Frontend/JavaScript/Intermediate/01 Distructing Array/app.ts
@@ -0,0 +1,9 @@
+console.log("uzair yasin");
+
+let resturant: ( String | Number | Boolean )[] = [
+    "uzair",
+    20,
+    true,
+    "yasin",
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+]
